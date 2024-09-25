81f3cdfe30e89a0fb8ca14b204591a998b52388d
Luis Adrian <122585598+luisc0230@users.noreply.github.com>
2024-09-25 16:09:59 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-//const cors = require('cors');
 
+// Routers
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var paidController = require('./controllers/paidController'); // Importar tu controlador
 
 var app = express();
 
@@ -20,6 +21,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/url', paidController.urlFlutter); // Asegura que la ruta /url esté configurada
+
+// Página de inicio - opcional
+app.get('/', function(req, res) {
+  res.send('Bienvenido a la página de inicio');
+});
 
 // Catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -28,6 +35,11 @@ app.use(function(req, res, next) {
 
 // Error handler
 app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // Send error response
   res.status(err.status || 500);
   res.json({
     message: err.message,
